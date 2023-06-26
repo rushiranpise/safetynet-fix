7542b122047fab99bd373f25d6ac4019adfdffa4
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2023-06-26 11:36:05 +0530

    Update ProxyProvider.kt

--- java/app/src/main/java/dev/kdrag0n/safetynetfix/proxy/ProxyProvider.kt
+++ java/app/src/main/java/dev/kdrag0n/safetynetfix/proxy/ProxyProvider.kt
@@ -42,8 +42,8 @@ class ProxyProvider(
             val origFingerprint = Build.FINGERPRINT
             val patchedFingerprint = "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
-            val origDEVICEINITIALSDKINT = Build.DEVICE_INITIAL_SDK_INT
-            val patchedDEVICEINITIALSDKINT = "25"
+            val origDEVICE_INITIAL_SDK_INT = Build.VERSION.DEVICE_INITIAL_SDK_INT
+            val patchedDEVICE_INITIAL_SDK_INT = "25"
 
             logDebug("Patch PRODUCT for KeyStore $origProduct -> $patchedProduct")
             Build::class.java.getDeclaredField("PRODUCT").let { field ->
@@ -75,10 +75,10 @@ class ProxyProvider(
                 field.isAccessible = true
                 field.set(null, patchedFingerprint)
             }
-            logDebug("Patch DEVICEINITIALSDKINT for KeyStore $origDEVICEINITIALSDKINT -> $patchedDEVICEINITIALSDKINT")
+            logDebug("Patch DEVICE_INITIAL_SDK_INT for KeyStore $origDEVICE_INITIAL_SDK_INT -> $patchedDEVICE_INITIAL_SDK_INT")
             Build::class.java.getDeclaredField("DEVICE_INITIAL_SDK_INT").let { field ->
                 field.isAccessible = true
-                field.set(null, patchedDEVICEINITIALSDKINT)
+                field.set(null, patchedDEVICE_INITIAL_SDK_INT)
             }
         }
         return super.getService(type, algorithm)
